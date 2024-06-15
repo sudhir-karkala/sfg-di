2638aa1c1e83fa9f9047066382dbffdc0ace7be8
sudhir-k <4837546+sudhir-karkala@users.noreply.github.com>
2024-06-15 23:22:08 +0530

    illustrate primary beans and profiles using java based configuration

--- src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -3,8 +3,8 @@ package guru.springframework.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Service("i18nService")
-@Profile({"EN", "default"})
+//@Service("i18nService")
+//@Profile({"EN", "default"})
 public class I18nEnglishGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
