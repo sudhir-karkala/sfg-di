8ee5454a06d67229a8a31a3e7b430720751bd20d
sudhir-k <4837546+sudhir-karkala@users.noreply.github.com>
2024-06-15 23:36:56 +0530

    illustrate DI in java based configuration

--- src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -1,11 +1,18 @@
 package guru.springframework.sfgdi.services;
 
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 //@Service("i18nService")
 //@Profile({"EN", "default"})
 public class I18nEnglishGreetingService implements GreetingService {
+    private final EnglishGreetingRepository englishGreetingRepository;
+
+    public I18nEnglishGreetingService(EnglishGreetingRepository englishGreetingRepository) {
+        this.englishGreetingRepository = englishGreetingRepository;
+    }
+
     @Override
     public String sayGreeting() {
         return "Hello World - EN";
