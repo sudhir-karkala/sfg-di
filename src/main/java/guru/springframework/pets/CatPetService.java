c38269b431413ba863d6c887336dc621deca2ce2
sudhir-k <4837546+sudhir-karkala@users.noreply.github.com>
2024-06-16 23:10:45 +0530

    illustrate component scan

--- src/main/java/guru/springframework/pets/CatPetService.java
+++ src/main/java/guru/springframework/pets/CatPetService.java
@@ -1,13 +1,13 @@
-package guru.springframework.sfgdi.services;
+package guru.springframework.pets;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile({"dog", "default"})
-public class DogPetService implements PetService {
+@Profile("cat")
+public class CatPetService implements PetService {
     @Override
     public String getPetType() {
-        return "Dogs are the best";
+        return "Cats are the best";
     }
 }
